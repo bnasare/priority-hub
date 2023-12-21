794a4c81a409b0630bf85386948581bae582b39e
bnasare <124492150+bnasare@users.noreply.github.com>
2023-12-21 20:57:15 +0000

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAZAP6cSzKNQcdYjPfq3v1rjziVRgsBS1E",
+    authDomain: "todo-87121.firebaseapp.com",
+    projectId: "todo-87121",
+    storageBucket: "todo-87121.appspot.com",
+    messagingSenderId: "846873837446",
+    appId: "1:846873837446:web:3b68e1ebb65ce31c517607"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 function Homepage() {
-    return <div>Homepage</div>;
+    return <div className="flex flex-col items-center justify-center h-screen m-0 overflow-hidden">
+        <input type="text" placeholder="Enter your name" className="w-1/4 border border-black" />
+    </div>;
 }
 
 export default Homepage;
